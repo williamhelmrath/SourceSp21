6fb3c2846f0b87295c904288a13d867d7e138070
William <william.helmrath@live.com>
2021-02-21 12:56:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Git repository we'll be using for Source in the Fall 2020 semester.
---
+++ Week_2/practice.js
@@ -0,0 +1,39 @@
+//1. Write a traditional function that converts fahrenheit to celsius
+console.log("test");
+//2. Modify your function for #1 to be arrow syntax
+
+//3. Write a one-line arrow function that takes in a number and returns a string stating whether the
+//   number is positive or negative using a ternary operator (assume the number will never be zero)
+
+//4. Write a switch statement for a 'day' variable that prints out something
+// based off of what day of the week it is
+// i.e. if its monday print "good luck" or wednesday print "hump day" or friday print "party"
+
+//5. Write a function that takes in a number, and uses a for loop to
+//return the sum of every number from 1 up to that number
+//ex. sumUp(7) = 1 + 2 + 3 + 4 + 5 + 6 + 7
+
+//6. Write a function that takes in a number, and prints out every number up to and including that value.
+//For each multiple of 3, instead print "fizz". For each multiple of 5, print "buzz". For numbers that
+//are multiples of 3 AND 5, print "fizzbuzz"
+//example output: 1, 2, fizz, 4, buzz, fizz,
+
+//7. Create an object for yourself with first Name, last Name,
+//   a list of three of your family members' names, and age
+
+//8. Write a function that takes in an array and prints out the amount of truthy values in that array
+//   using .forEach()
+const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+//9. Using the map function and arrow syntax, return an array of objects
+//   that contain a fullName field and an averageGrade field representing
+//   the letter grade that corresponds to their GPA
+const attendance = [
+  { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
+  { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
+  { firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
+  { firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
+  { firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
+  { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
+  { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
+];
